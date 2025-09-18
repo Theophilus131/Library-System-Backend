189cb1eb6afaf353f404341e515d1840c3ff61ac
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-18 05:09:51 +0100

    Add BookRequest DTO to handle book-related requestsAdd BookRequest and BookResponse DTO classes to handle book data transfers

---
+++ src/main/java/com/library/management/system/dtos/response/BookResponse.java
@@ -0,0 +1,12 @@
+package com.library.management.system.dtos.response;
+
+
+@lombok.Data
+public class BookResponse {
+    private String bookName;
+    private String authorName;
+    private String publisherName;
+    private String categoryName;
+    private String ISBN;
+    private String bookId;
+}
