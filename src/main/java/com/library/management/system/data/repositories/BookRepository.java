33276ca7792fe36b857c5f65fc0b021b97d6f705
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 00:09:58 +0100

    Update BookRepository to add @Repository annotation and replace findByBookId with findById

--- src/main/java/com/library/management/system/data/repositories/BookRepository.java
+++ src/main/java/com/library/management/system/data/repositories/BookRepository.java
@@ -2,7 +2,12 @@ package com.library.management.system.data.repositories;
 
 import com.library.management.system.data.models.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BookRepository extends MongoRepository<Book,String>{
-    Book findByBookId(String bookId);
+
+    Optional<Book> findById(String id);
 }
