5f52fe67291e12bf20da33f8c7bc1db88726764f
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 00:09:44 +0100

    Add additional methods to BookService interface for enhanced book operations

--- src/main/java/com/library/management/system/services/BookService.java
+++ src/main/java/com/library/management/system/services/BookService.java
@@ -1,4 +1,24 @@
 package com.library.management.system.services;
 
+import com.library.management.system.data.models.Book;
+import com.library.management.system.dtos.request.BookRequest;
+import com.library.management.system.dtos.response.BookResponse;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookService {
+    BookResponse addBook(BookRequest bookRequest);
+    BookResponse updateBook(BookRequest bookRequest);
+    BookResponse deleteBook(BookRequest bookRequest);
+    BookResponse getBook(BookRequest bookRequest);
+    BookResponse getBookByISBN(BookRequest bookRequest);
+    BookResponse getBookByBookName(BookRequest bookRequest);
+    BookResponse getBookByAuthorName(BookRequest bookRequest);
+    BookResponse getBookByPublisherName(BookRequest bookRequest);
+
+
+    List<Book> getAllBook();
+    Optional<Book> getBookById(String id);
+    void deleteBookById(String id);
 }
