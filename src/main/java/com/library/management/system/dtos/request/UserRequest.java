a9c0c0234ff017e86b3a87d6186e353c6d13dae2
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 00:10:47 +0100

    Add `email` field to UserRequest DTO for enhanced user data handling

--- src/main/java/com/library/management/system/dtos/request/UserRequest.java
+++ src/main/java/com/library/management/system/dtos/request/UserRequest.java
@@ -3,7 +3,9 @@ package com.library.management.system.dtos.request;
 
 @lombok.Data
 public class UserRequest {
+
     private String userName;
+    private String email;
     private String password;
     private String role;
 }
