d76ee3e58c294cddd25ab8d233c1717542ff4a12
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 01:28:33 +0100

    Add methods to BookRepository for filtering by status and category name

--- src/main/java/com/library/management/system/data/repositories/BookRepository.java
+++ src/main/java/com/library/management/system/data/repositories/BookRepository.java
@@ -4,10 +4,12 @@ import com.library.management.system.data.models.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends MongoRepository<Book,String>{
 
-    Optional<Book> findById(String id);
+   List<Book> findByStatus(String status);
+    List<Book> findByCategoryName(String categoryName);
 }
