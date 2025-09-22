c2a6ae9343a18dd274c892ffab4984f6b0e07085
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 01:27:49 +0100

    Add methods to UserService interface for user registration and login

--- src/main/java/com/library/management/system/services/UserService.java
+++ src/main/java/com/library/management/system/services/UserService.java
@@ -1,4 +1,9 @@
 package com.library.management.system.services;
 
+import com.library.management.system.dtos.request.UserRequest;
+import com.library.management.system.dtos.response.UserResponse;
+
 public interface UserService {
+    UserResponse registerUser(UserRequest userRequest);
+    UserResponse loginUser(UserRequest userRequest);
 }
