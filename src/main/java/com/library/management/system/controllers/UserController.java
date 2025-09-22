cb326d890bd8d320fc7ab4f1fc5be62eec1be08d
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 01:29:22 +0100

    Add UserController with endpoints for user registration and login

---
+++ src/main/java/com/library/management/system/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.library.management.system.controllers;
+
+import com.library.management.system.dtos.request.UserRequest;
+import com.library.management.system.dtos.response.UserResponse;
+import com.library.management.system.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/register")
+    public UserResponse registerUser(UserRequest userRequest){
+        return userService.registerUser(userRequest);
+    }
+
+    @PostMapping("/login")
+    public UserResponse loginUser(UserRequest userRequest){
+        return userService.loginUser(userRequest);
+    }
+}
