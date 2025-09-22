67a9b156f085390c5bbabdcc0d98749c20a0f3c0
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 01:29:50 +0100

    Add UserResponse DTO and UserRepository interface for user management

---
+++ src/main/java/com/library/management/system/dtos/response/UserResponse.java
@@ -0,0 +1,9 @@
+package com.library.management.system.dtos.response;
+
+@lombok.Data
+public class UserResponse {
+    private String userName;
+    private String email;
+    private String role;
+    private String userId;
+}
