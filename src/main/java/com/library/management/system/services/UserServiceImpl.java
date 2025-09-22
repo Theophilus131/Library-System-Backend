2e4d214a41e9f80e670936cfa5a1beae6032fa41
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-22 01:28:01 +0100

    Add UserServiceImpl methods for user registration and login

--- src/main/java/com/library/management/system/services/UserServiceImpl.java
+++ src/main/java/com/library/management/system/services/UserServiceImpl.java
@@ -1,4 +1,42 @@
 package com.library.management.system.services;
 
+import com.library.management.system.data.models.User;
+import com.library.management.system.data.repositories.UserRepository;
+import com.library.management.system.dtos.request.UserRequest;
+import com.library.management.system.dtos.response.UserResponse;
+import com.library.management.system.mappers.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService{
+
+   private final UserRepository userRepository;
+
+   @Autowired
+    public UserServiceImpl(UserRepository userRepository){
+      this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserResponse registerUser(UserRequest userRequest){
+       if(userRepository.existsByEmail(userRequest.getEmail())){
+           throw new RuntimeException("Email already registered");
+       }
+       User user = UserMapper.toEntity(userRequest);
+       userRepository.save(user);
+       return UserMapper.toResponse(user);
+    }
+
+    @Override
+    public UserResponse loginUser(UserRequest userRequest){
+        User user = userRepository.finfbyEmail(userRequest.getEmail()).orElseThrow(()->new RuntimeException("User not found"));
+        if(!user.getPassword().equals(userRequest.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+        return UserMapper.toResponse(user);
+    }
+
+
+
 }
