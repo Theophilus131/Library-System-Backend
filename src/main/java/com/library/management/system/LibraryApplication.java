17f5e611dd0f9204bbb3e6539803f04c7c8d382d
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-09-30 03:20:31 +0100

    Add `LibraryApplication` entry point and `BookController` with CRUD endpoints

---
+++ src/main/java/com/library/management/system/LibraryApplication.java
@@ -0,0 +1,12 @@
+package com.library.management.system;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
