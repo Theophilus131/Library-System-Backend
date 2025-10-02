87922ce23ec6af4addcc58a8385dde72ed768ee2
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-10-02 04:23:50 +0100

    Fix: Add `@RequestBody` annotation to `UserController` methods for JSON deserialization

--- src/main/java/com/library/management/system/controllers/UserController.java
+++ src/main/java/com/library/management/system/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.library.management.system.dtos.response.UserResponse;
 import com.library.management.system.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,13 @@ public class UserController {
 
 
     @PostMapping("/register")
-    public UserResponse registerUser(UserRequest userRequest){
+    public UserResponse registerUser(@RequestBody UserRequest userRequest){
 
         return userService.registerUser(userRequest);
     }
 
     @PostMapping("/login")
-    public UserResponse loginUser(UserRequest userRequest){
+    public UserResponse loginUser(@RequestBody UserRequest userRequest){
         return userService.loginUser(userRequest);
     }
 }
