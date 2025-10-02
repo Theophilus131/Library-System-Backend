38dfa9c3f0367d586b318eaf87bc9577058f975a
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-10-02 04:24:15 +0100

    Fix: Improve password handling and add `EmailAlreadyExistsException` in `UserServiceImpl`

--- src/main/java/com/library/management/system/services/UserServiceImpl.java
+++ src/main/java/com/library/management/system/services/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.library.management.system.data.repositories.UserRepository;
 import com.library.management.system.dtos.request.UserRequest;
 import com.library.management.system.dtos.response.UserResponse;
 import com.library.management.system.mappers.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.library.management.system.exceptions.EmailAlreadyExistsException;
 
-
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService{
 
@@ -25,20 +27,33 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserResponse registerUser(UserRequest userRequest){
-       if(userRepository.existsByEmail(userRequest.getEmail())){
-           throw new RuntimeException("Email already registered");
+       User existingUser = userRepository.findByEmail(userRequest.getEmail()).orElse(null);
+//       log.info("Existing user: {}",existingUser);
+//        System.out.println("1111111");
+//        System.out.println(existingUser);
+
+       if(existingUser != null){
+           throw new EmailAlreadyExistsException("Email already registered");
        }
+
+
        User user = UserMapper.toEntity(userRequest);
-       userRepository.save(user);
-       return UserMapper.toResponse(user);
+
+       user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+
+       User savedUser = userRepository.save(user);
+       return UserMapper.toResponse(savedUser);
     }
 
     @Override
     public UserResponse loginUser(UserRequest userRequest){
         User user = userRepository.findByEmail(userRequest.getEmail()).orElseThrow(()->new RuntimeException("User not found"));
-        if(!user.getPassword().equals(userRequest.getPassword())){
+
+
+        if(!passwordEncoder.matches(userRequest.getPassword(),user.getPassword())){
             throw new RuntimeException("Invalid password");
         }
+
         return UserMapper.toResponse(user);
     }
 
