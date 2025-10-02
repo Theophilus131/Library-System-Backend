fcb7fc7eccec923dc99ad47bb8f90e97d9b1862e
Theophilus danladi umar <theophilusumar27@gmail.com>
2025-10-02 05:09:15 +0100

    Add `EmailAlreadyExistsException` and `BookNotFoundException` classes for custom exception handling

---
+++ src/main/java/com/library/management/system/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.library.management.system.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+
+        super(message);
+    }
+}
+
+
